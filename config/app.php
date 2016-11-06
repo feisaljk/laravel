87a0d5b7470ab185ed44eefdef1c56cbd8af6d8e
feisaljk <feisal.jamaludin22@gmail.com>
2016-11-07 01:03:08 +0700

    Twitter Apps Beta Version

--- config/app.php
+++ config/app.php
@@ -52,8 +52,8 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
-
+    'timezone' => 'Asia/Jakarta',
+    
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
